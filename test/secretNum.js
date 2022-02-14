c3bb657749192d9441853c85e78531079801e5ed
Tharoon Balaji <tharoonbalaji@Tharoons-Air.lan>
2022-02-14 15:48:20 -0500

    secretNum

---
+++ test/secretNum.js
@@ -0,0 +1,13 @@
+
+const { ethers } = require ("hardhat");
+
+
+console.log("yes");
+let hsh= "0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365";
+
+   for(let i =0; i<256; i++){
+      if(hsh.includes(ethers.utils.keccak256([i]))){
+        console.log(i);
+      }
+   }
+   
