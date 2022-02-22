4dbe77ee2761aaa028b4513a00b0158e157181cc
Tharoon Balaji <tharoonbalaji@gmail.com>
2022-02-21 20:27:41 -0500

    changes

--- contracts/newNum.sol
+++ contracts/newNum.sol
@@ -5,7 +5,7 @@ contract GuessTheNewNumber {
        // proxy contract
        // connect to newNum challenge contract to run the contract to find answer then submit answer 
         uint8 answer = uint8(keccak256(block.blockhash(block.number - 1), now));
-        uint8 n // int to be found as answer
+        uint8 n; // int to be found as answer
         if (n == answer) {
             msg.sender.transfer(2 ether);
         }
